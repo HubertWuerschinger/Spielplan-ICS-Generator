2416ac061b21154eeb0658d7ebd6c8b6d08d4d77
Hubert Würschinger <61906395+HubertWuerschinger@users.noreply.github.com>
2024-03-02 23:13:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,11 @@ from PIL import Image
 import io
 import re
 
-
+# Funktion zur Bereinigung von nicht-druckbaren Zeichen
+def remove_non_printable_chars(text):
+    # Entfernt alle Zeichen, die in Excel-Tabellen nicht zulässig sind
+    # Hier werden alle Zeichen außer druckbaren ASCII-Zeichen entfernt
+    return re.sub(r'[^\x20-\x7E]', '', text)
 
 # OCR-Konfiguration
 # Hinweis: Ändern Sie den Pfad entsprechend Ihrer Tesseract-Installation
@@ -19,35 +23,20 @@ st.title("Text- und Zahlen-Erkennung")
 uploaded_file = st.file_uploader("Bild hochladen", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Hochgeladenes Bild', use_column_width=True)
-    
-    # Bild in Text umwandeln
+# Bild in Text umwandeln
     text = pytesseract.image_to_string(image)
+
+    # Bereinigen Sie den Text von nicht-druckbaren Zeichen
     cleaned_text = remove_non_printable_chars(text)
 
     st.write("Erkannter Text:")
     st.write(cleaned_text)
 
     # Text in DataFrame umwandeln
-    def remove_illegal_chars_for_excel(text):
-        # Entfernt alle Zeichen, die in Excel-Tabellen nicht zulässig sind
-        # Hier werden alle Zeichen außer druckbaren ASCII-Zeichen entfernt
-        return re.sub(r'[^\x20-\x7E]', '', text)
-    #data = {'Text': cleaned_text.split('\n')}
-    
-    
-
-    # Angenommen, 'text' enthält Ihren OCR-erfassten Text
-    cleaned_text = remove_illegal_chars_for_excel(text)
-
-    # Erstellen Sie die DataFrame mit dem bereinigten Text
     data = {'Text': cleaned_text.split('\n')}
     df = pd.DataFrame(data)
     st.write(df)
 
-
-    
     # Excel-Download
     towrite = io.BytesIO()
     df.to_excel(towrite, index=False, header=True)
