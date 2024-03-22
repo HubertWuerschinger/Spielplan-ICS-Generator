9f5d89c3208b440fe03664b4f2c6ba7bf778234d
Hubert Würschinger <61906395+HubertWuerschinger@users.noreply.github.com>
2024-03-22 18:36:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def process_schedule(text):
             except Exception as e:
                 st.error(f"Fehler beim Parsen der Zeile: {line} - {e}")
                 continue
-   return events
+       return events
 
 
 
